82f32c02e04746170fba6de644509636dc5241e4
Leah <25106569+RealFX-Code@users.noreply.github.com>
2026-04-24 19:50:46 +0200

    SusFS & KernelSU: e02a793d7a23f3c0c8cfcc1e068037921e3dabca
    SusFS: https://gitlab.com/simonpunk/susfs4ksu/-/commit/e02a793d7a23f3c0c8cfcc1e068037921e3dabca
    KernelSU: https://github.com/tiann/KernelSU/commit/123040998c4042971a865f0cfe9ec401559a77b1

--- fs/exec.c
+++ fs/exec.c
@@ -1947,7 +1947,7 @@ out_unmark:
 
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_su_compat_enabled __read_mostly;
-extern struct static_key_false susfs_set_sdcard_android_data_decrypted_key_false;
+extern struct static_key_true susfs_is_sdcard_android_data_not_decrypted;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
@@ -1971,7 +1971,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		goto orig_flow;
 	}
 
-	if (static_branch_unlikely(&susfs_set_sdcard_android_data_decrypted_key_false))
+	if (static_branch_unlikely(&susfs_is_sdcard_android_data_not_decrypted))
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
